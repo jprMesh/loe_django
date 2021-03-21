d6e8213e54ece409227250844578bd9e0d295c15
jprMesh <jprmesh@gmail.com>
2021-03-21 01:20:03 -0700

    first views! extremely basic leaderboard view added.

--- loe/ratings/views.py
+++ loe/ratings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.db.models import Avg
 
-# Create your views here.
+from .models import Prediction
+
+
+def index(request):
+    return HttpResponse("Nothing here yet. Go to /leaderboard")
+
+
+def leaderboard(request):
+    brier_leaderboard = Prediction.objects.all().values('user__username').annotate(brier=Avg('brier')).order_by('brier')[:20]
+    template = loader.get_template('ratings/leaderboard.html')
+    context = {
+        'leaderboard': brier_leaderboard,
+    }
+    return HttpResponse(template.render(context, request))
