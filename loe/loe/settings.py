c3ed112e4fd776979cefb1eb1cd19c6c964bf384
jprMesh <jprmesh@gmail.com>
2021-04-01 05:43:52 +0000

    migrate db to postgres

--- loe/loe/settings.py
+++ loe/loe/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'loe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': config('DJANGO_POSTGRES_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
