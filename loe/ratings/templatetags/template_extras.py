6f125095cd8170102e84179a451c60c4c33d2ade
jprMesh <jprmesh@gmail.com>
2021-04-03 17:59:02 -0700

    load images from db_saved file

--- loe/ratings/templatetags/template_extras.py
+++ loe/ratings/templatetags/template_extras.py
@@ -9,8 +9,10 @@ register = template.Library()
 
 @register.inclusion_tag('prediction_tr.html')
 def prediction_tr(match_pk, username):
-    match = Match.objects.filter(pk=match_pk).values(
-        'pk', 'team1__team_name', 'team1__short_name', 'team1_score', 'team2__team_name', 'team2__short_name', 'team2_score', 'best_of', 'region', 'start_timestamp', 'elo_processed').first()
+    match = Match.objects.filter(pk=match_pk).values('pk',
+        'team1__team_name', 'team1__short_name', 'team1__logo_img', 'team1_score',
+        'team2__team_name', 'team2__short_name', 'team2__logo_img', 'team2_score',
+        'best_of', 'region', 'start_timestamp', 'elo_processed').first()
     context = {'match': match}
     context['match_started'] = match['start_timestamp'] < (timezone.now() - timedelta(hours=1))
     context['match_complete'] = match['elo_processed']
