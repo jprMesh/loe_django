792fcbfb52d99db38782fcc398ecd96d07a4dd31
jprMesh <jprmesh@gmail.com>
2021-04-24 07:08:48 +0000

    fixed log path for match updates

--- loe/ratings/management/commands/regular_match_update.sh
+++ loe/ratings/management/commands/regular_match_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-date | tee -a /home/loe_django/loe/log/pop_match.log /home/loe_django/log/calc_elo.log
+date | tee -a /home/loe_django/loe/log/pop_match.log /home/loe_django/loe/log/calc_elo.log
 cd /home/loe_django/loe && /home/loe_django/env/bin/python manage.py populate_matches 2021 >> log/pop_match.log
 cd /home/loe_django/loe && /home/loe_django/env/bin/python manage.py calculate_elo >> log/calc_elo.log
