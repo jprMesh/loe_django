71311aa455e6442853848401d54af833c1a16f6b
Ben McMorran <ben.mcmorran@gmail.com>
2021-04-29 23:41:08 -0700

    Mostly working Docker container

---
+++ docker/entrypoint.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+service postgresql start
+. /venv/bin/activate
+cd /src/loe
+python manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py populate_teams
+# TODO: enable once I know how these should work
+# python manage.py populate_matches
+# python manage.py createsuperuser
+# python manage.py shell
+
+# Explicitly specify 0.0.0.0 to bind on all network interfaces. Without this
+# Django will only listen on loopback and won't be able to talk to the host.
+python manage.py runserver 0.0.0.0:8000
