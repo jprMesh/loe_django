70504259b9707179e87746ac6717d48dc621bb57
Ben McMorran <ben.mcmorran@gmail.com>
2021-04-30 22:38:53 -0700

    Add instructions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,10 +5,12 @@ cd /src/loe
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py populate_teams
+
 # TODO: enable once I know how these should work
 # python manage.py populate_matches
 # python manage.py createsuperuser
 # python manage.py shell
+# python manage.py calcuate_elo
 
 # Explicitly specify 0.0.0.0 to bind on all network interfaces. Without this
 # Django will only listen on loopback and won't be able to talk to the host.
